95495c570db315b2368853fa0f1f4b68925611ef
reshaffa <reshaffa@gmail.com>
2022-09-25 22:36:31 +0700

    Initial Config - 25/09/2022

---
+++ application/config/autoload.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+$autoload['libraries'] = array('database','session','table','logs','template');
+$autoload['drivers'] = array();
+$autoload['helper'] = array('url','form','html','string','text','security');
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
